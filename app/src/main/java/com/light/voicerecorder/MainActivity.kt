c5577cdeba9980f29c4c3ae56dbc229c544c43c2
home <https://github.com/vladzavdv16/hogwartslibrary.git>
2021-09-07 22:28:52 +0300

    adding in navigation to the app

--- app/src/main/java/com/light/voicerecorder/MainActivity.kt
+++ app/src/main/java/com/light/voicerecorder/MainActivity.kt
@@ -2,10 +2,21 @@ package com.light.voicerecorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.light.voicerecorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigation, Navigation.findNavController(this, R.id.fragment)
+        )
     }
 }
---
+++ app/src/main/java/com/light/voicerecorder/record/RecordFragment.kt
@@ -0,0 +1,23 @@
+package com.light.voicerecorder.record
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.light.voicerecorder.R
+
+
+class RecordFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_record, container, false)
+    }
+
+
+}
